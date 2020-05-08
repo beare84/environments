5ec899332e6eca80a342c9d2b6feb2d1e3a7533f
Nick Bean <you@example.com>
2020-05-08 14:51:51 +1000

    Added information about splunk free licence

--- production/manifests/profiles/splunkenterprise.pp
+++ production/manifests/profiles/splunkenterprise.pp
@@ -3,6 +3,7 @@ class profiles::splunkenterprise (
   # In the Splunk Web UI you can point Splunk to look for logs from a directory (Settings > Data Inputs > Files anmd Directories).
   # My Splunk Enterprise server is also my rsyslog server so it can get all client logs from /srv/log
   # You'll also need to activate the Free licence as Splunk comes installed with trial licence (Setting > Licening > Change Licence Group > Free Licence)
+  # The Free license does not expire. The Free license allows you to index 500 MB per day
 
   # The below code changes the admin user's password to 'password'
   # I generated this hash on a splunk enterprise server using:
