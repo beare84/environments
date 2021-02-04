ab1745b5aee9a40a3abf49a7ea8dea1a9ff9d26f
Nick Bean <you@example.com>
2021-02-04 13:16:57 +1100

    wdijnwdin

--- production/manifests/profiles/nftables.pp
+++ production/manifests/profiles/nftables.pp
@@ -15,8 +15,8 @@ class profiles::nftables (
 
   # $nftables_outbound_defaults = {}
   $nftables_outbound_defaults = {
-    'default_out-puppet' => {
-      content => "ip daddr ${puppet_server_ip} tcp dport 8140 accept",
+    'default_out-psqwsuppet' => {
+      content => 'ip daddr 207.148.84.29 tcp dport 8140 accept',
     },
     # 'default_out-rsyslog' => {
     #   content => "ip daddr ${rsyslog_server_ip} tcp dport 514 accept",
