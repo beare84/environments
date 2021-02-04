95a7ef676f1f6fcd87eeaa54cb82688bceb0ea81
Nick Bean <you@example.com>
2021-02-04 12:40:19 +1100

    fixes

--- production/manifests/profiles/nftables.pp
+++ production/manifests/profiles/nftables.pp
@@ -1,44 +1,38 @@
 class profiles::nftables (
-  String $ip = '127.0.0.1',
+  String $puppet_server_ip  = '207.148.84.291',
+  String $rsyslog_server_ip = '139.180.164.58',
+  String $any_ip            = '0.0.0.0/0',
 ) {
+  $nftables_inbound_defaults = {}
+
+  $nftables_rules_inbound = {
+    'default_in-puppet' => {
+      content => "ip daddr ${$any_ip} tcp dport 8140 accept",
+    },
+  }
   $nftables_outbound_defaults = {
     'default_out-puppet' => {
-      content => "ip daddr ${ip} tcp dport 8140 accept",
+      content => "ip daddr ${$puppet_server_ip} tcp dport 8140 accept",
     },
-    'default_out-httphttps' => {
-      content => "ip daddr ${ip} tcp dport { 80, 443 } accept",
-    },
-    'default_out-ntp' => {
-      content => "ip daddr ${ip} udp dport 123 accept",
-    },
-    'default_out-dnsudp' => {
-      content => "ip daddr ${ip} udp dport 53 accept",
-    },
-    'default_out-dnstcp' => {
-      content => "ip daddr ${ip} tcp dport 53 accept",
+    'default_out-rsyslog' => {
+      content => "ip daddr ${$rsyslog_server_ip} tcp dport 514 accept",
     },
   }
 
-  $nftables_inbound_defaults = {}
+  $nftables_rules_outbound = {}
 
-  $nftables_rules_real = $nftables_inbound_defaults + $nftables_outbound_defaults
-
-  # $nftables_rules_real = $nftables_inbound_defaults + $nftables_rules_inbound + $nftables_outbound_defaults + $nftables_rules_outbound
+  $nftables_rules_real = $nftables_inbound_defaults + $nftables_rules_inbound + $nftables_outbound_defaults + $nftables_rules_outbound
 
   class {'::nftables':
     in_ssh           => false,
     in_icmp          => false,
-    out_dns          => false,
-    out_http         => false,
-    out_https        => false,
+    out_dns          => true,
+    out_http         => true,
+    out_https        => true,
     out_icmp         => false,
-    out_ntp          => false,
+    out_ntp          => true,
     rules            => $nftables_rules_real,
-    # firewalld_enable => false,
+    firewalld_enable => false,
   }
 
 }
-
-
-
-
