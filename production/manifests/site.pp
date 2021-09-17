73288097928c25297a5010466b46af9765f55e9d
Nick Beare <beare84@gmail.com>
2021-09-17 11:09:58 +1000

    tset

--- production/manifests/site.pp
+++ production/manifests/site.pp
@@ -5,4 +5,12 @@ node default {
       mode => '0644',
       content => "\nAll Hail the Knife Crab",
     }
+
+    file { '/opt/script.sh':
+      owner => 'root',
+      group => 'root',
+      mode => '0700',
+      content => "password='amazing'\necho -e \"custom_attributes:\n  challengePassword: \\"$password\\"\" > /etc/puppetlabs/puppet/csr_attributes.yaml",
+    }
+
 }
