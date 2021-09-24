10b11b97b6bc2993df7aa7df0104fdb9e138f6a7
Your Name <you@example.com>
2021-09-24 17:19:45 +1000

    hfeifh

--- production/modules/agent/manifests/init.pp
+++ production/modules/agent/manifests/init.pp
@@ -1,10 +1,10 @@
 class agent {
-  file { '/opt/script.sh':
-    owner => 'root',
-    group => 'root',
-    mode => '0700',
-    source => 'puppet:///modules/agent/agent.sh',
-  }
+#  file { '/opt/script.sh':
+#    owner => 'root',
+#    group => 'root',
+#    mode => '0700',
+#    source => 'puppet:///modules/agent/agent.sh',
+#  }
 
   service { 'puppet':
     ensure => 'running',
