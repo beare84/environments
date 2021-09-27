797f3ade2d41fc61043b4004afab1345316ada53
Nick Beare <beare84@gmail.com>
2021-09-27 15:56:31 +1000

    test

--- production/modules/apache/manifests/install.pp
+++ production/modules/apache/manifests/install.pp
@@ -4,11 +4,12 @@
 #
 # @example
 #   include apache::install
-class apache::install (
-  $install_name   = $apache::params::install_name,
-  $install_ensure = $apache::params::install_ensure,
-) inherits apache::params {
-  package { "${install_name}":
-    ensure => "${install_ensure}",
+# class apache::install (
+#   $install_name   = $apache::params::install_name,
+#   $install_ensure = $apache::params::install_ensure,
+# ) inherits apache::params {
+class apache::install {
+  package { "${apache::install_name}":
+    ensure => "${apache::install_ensure}",
   }
 }
