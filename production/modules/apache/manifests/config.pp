ccfd7ef05d0d83cc8dada80b21bfaffee22d4a21
Nick Beare <beare84@gmail.com>
2021-09-27 16:23:23 +1000

    test

--- production/modules/apache/manifests/config.pp
+++ production/modules/apache/manifests/config.pp
@@ -5,4 +5,12 @@
 # @example
 #   include apache::config
 class apache::config {
+  file { 'apache_config':
+    ensure => $apache::config_ensure,
+    path   => $apache::config_path,
+    source => "puppet:///modules/apache/${osfamily}.conf",
+    mode   => '0600',
+    owner  => 'root',
+    group  => 'root',
+  }
 }
