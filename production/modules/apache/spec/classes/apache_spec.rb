619d0e157bbedf6c067831b5d4b14be5bad0402e
Nick Beare <beare84@gmail.com>
2021-09-29 16:17:15 +1000

    test

--- production/modules/apache/spec/classes/apache_spec.rb
+++ production/modules/apache/spec/classes/apache_spec.rb
@@ -3,20 +3,17 @@
 require 'spec_helper'
 
 describe 'apache' do
-  on_supported_os.each do |os, os_facts|
-    context "on #{os}" do
-      let(:facts) { os_facts }
-      it { is_expected.to contain_class('apache::install') }
-      it { is_expected.to contain_class('apache::config') }
-      it { is_expected.to contain_class('apache::service') }
-      it { is_expected.to compile }
+  # on_supported_os.each do |os, os_facts|
+  #   context "on #{os}" do
+  #     let(:facts) { os_facts }
+  it { is_expected.to contain_class('apache::install') }
+  it { is_expected.to contain_class('apache::config') }
+  it { is_expected.to contain_class('apache::service') }
+  it { is_expected.to compile }
 
-      case os_facts[:osfamily]
-      when 'RedHat'
-        it { is_expected.to contain_package{'httpd').with(ensure: 'present') }
-        it { is_expected.to contain_service{'httpd').with(ensure: 'runnning', enable: true, hasrestart: true ) }
-      }
-
-    end
-  end
+  case os_facts[:osfamily]
+  when 'RedHat'
+    it { is_expected.to contain_package{'httpd').with(ensure: 'present') }
+    it { is_expected.to contain_service{'httpd').with(ensure: 'runnning', enable: true, hasrestart: true ) }
+  }
 end
