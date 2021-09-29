8761330b688fc96532ee6f49a000a007066783f8
Nick Beare <beare84@gmail.com>
2021-09-29 17:06:29 +1000

    test

--- production/modules/apache/spec/classes/apache_spec.rb
+++ production/modules/apache/spec/classes/apache_spec.rb
@@ -6,13 +6,15 @@ describe 'apache' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-
+      it { is_expected.to contain_class('apache::install') }
+      it { is_expected.to contain_class('apache::config') }
+      it { is_expected.to contain_class('apache::service') }
       it { is_expected.to compile }
 
       case os_facts[:osfamily]
       when 'RedHat'
         it { is_expected.to contain_package('httpd').with(ensure: 'present') }
-        it { is_expected.to contain_service('httpd').with(ensure: 'runnning', enable: true, hasrestart: true ) }
+        it { is_expected.to contain_service('httpd').with(ensure: 'running', enable: true, hasrestart: true ) }
       end
     end
   end
