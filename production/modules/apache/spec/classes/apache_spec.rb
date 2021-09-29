6c2a68bcb079bdabc1be4965e6961df17633122b
Nick Beare <beare84@gmail.com>
2021-09-29 16:55:58 +1000

    test

--- production/modules/apache/spec/classes/apache_spec.rb
+++ production/modules/apache/spec/classes/apache_spec.rb
@@ -2,22 +2,12 @@
 
 require 'spec_helper'
 
-describe 'apache::config' do
+describe 'apache' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      it { is_expected.to contain_class('apache::install') }
-      it { is_expected.to contain_class('apache::config') }
-      it { is_expected.to contain_class('apache::service') }
       it { is_expected.to compile }
-    
-      case os_facts[:osfamily]
-      when 'RedHat'
-        it { is_expected.to contain_package('httpd').with(ensure: 'present') }
-        it { is_expected.to contain_service('httpd').with(ensure: 'runnning', enable: true, hasrestart: true ) }
-      end
-
     end
   end
 end
