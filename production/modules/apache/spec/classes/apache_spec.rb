fff3980e64b2fe7fe12f28907e6e3f664aaa19fe
Nick Beare <beare84@gmail.com>
2021-09-29 17:02:49 +1000

    test

--- production/modules/apache/spec/classes/apache_spec.rb
+++ production/modules/apache/spec/classes/apache_spec.rb
@@ -8,6 +8,12 @@ describe 'apache' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+
+      case os_facts[:osfamily]
+      when 'RedHat'
+        it { is_expected.to contain_package('httpd').with(ensure: 'present') }
+        it { is_expected.to contain_service('httpd').with(ensure: 'runnning', enable: true, hasrestart: true ) }
+      end
     end
   end
 end
