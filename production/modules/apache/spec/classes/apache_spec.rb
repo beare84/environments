befc40050698bfb288efdcc6127beccdac1d29ae
Nick Beare <beare84@gmail.com>
2021-09-29 16:24:30 +1000

    test

--- production/modules/apache/spec/classes/apache_spec.rb
+++ production/modules/apache/spec/classes/apache_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe 'apache' do
   # on_supported_os.each do |os, os_facts|
   #   context "on #{os}" do
-  #     let(:facts) { os_facts }
+  let(:facts) { os_facts }
   it { is_expected.to contain_class('apache::install') }
   it { is_expected.to contain_class('apache::config') }
   it { is_expected.to contain_class('apache::service') }
